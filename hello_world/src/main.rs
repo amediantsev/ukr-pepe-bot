d46a164fbd900fc0692a09c4f10a4483afc0f264
amediantsev <zencrazycat@gmail.com>
2023-05-04 10:34:52 +0300

    Clean up

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,7 +1,5 @@
 use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 
-/// This is the main body for the function.
-/// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
@@ -12,8 +10,6 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
         .unwrap_or("world");
     let message = format!("{{\"message\": \"Hello {who}, this is an AWS Lambda HTTP request\"}}");
 
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
     let resp = Response::builder()
         .status(200)
         .header("content-type", "application/json")
@@ -26,9 +22,7 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
-        // disable printing the name of the module in every log line.
         .with_target(false)
-        // disabling time is handy because CloudWatch will add the ingestion time.
         .without_time()
         .init();
 
