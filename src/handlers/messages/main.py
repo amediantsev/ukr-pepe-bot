23fdd1e54b952cb3bcf1c9d7191390af8284fa81
amediantsev <zencrazycat@gmail.com>
2023-05-16 09:59:05 +0300

    First implementation of conversations.

--- src/handlers/messages/main.py
+++ src/handlers/messages/main.py
@@ -1,19 +1,20 @@
-import functools
 import json
 import os
 import random
 from http import HTTPStatus
 
 from aws_lambda_powertools import Logger
-from telegram import Update
+from telegram import Update, Message
 from telegram.constants import CHAT_GROUP, CHAT_SUPERGROUP
 
-from pepe import generate_pepe_message
+from pepe import generate_pepe_message, PEPE_ALIASES
 from tg import bot
 from decorators import handle_errors
+import aws.dynamodb as dynamodb_operations
 
 logger = Logger()
 
+GPT_CONTEXT_LENGTH = 6
 ADMIN_IDS = os.getenv("ADMIN_IDS", "").split(",")
 # EBOSHIM_MOSHСNO_CHAT_ID = "-1001318344639"
 SINGLE_MESSAGE_USER_PROMPT = "Напиши повідомлення довжиною від 5 до 30 слів у продовження до бесіди :\n"
@@ -35,35 +36,63 @@ USERNAMES_TO_UKR = {
 }
 
 
-@handle_errors
-def handler(event, _):
-    update = Update.de_json(json.loads(event.get("body")), bot)
-    if any(
+def skip_update(update: Update) -> bool:
+    return any(
         (
             not update.message,
             (
                 update.message.chat.type not in (CHAT_GROUP, CHAT_SUPERGROUP)
                 and str(update.message.chat_id) not in ADMIN_IDS
             ),
+            len(update.message.text) > 200,
         )
-    ):
+    )
+
+
+def send_message_triggered(message: Message):
+    message_text = message.text.lower()
+    return any(
+        (
+            message.reply_to_message and message.reply_to_message.from_user.id == bot.id,
+            any(filter(lambda pepe_alias: pepe_alias.match(message_text), PEPE_ALIASES)),
+            random.random() < 0.07,
+        )
+    )
+
+
+@handle_errors
+def handler(event, _):
+    update = Update.de_json(json.loads(event.get("body") or "{}"), bot)
+    if skip_update(update):
         return {"statusCode": HTTPStatus.OK}
     logger.info(update)
-    messages = [
-        {
-            "role": "user",
-            "name": USERNAMES.get(update.message.from_user.id, "Павло"),
-            # "content": f"{RANDOM_SINGLE_MESSAGE_USER_PROMPT} у відповідь на `{update.message.text}`",
-            "content": update.message.text,
-        }
-    ]
-    if (
-        update.message.reply_to_message and update.message.reply_to_message.from_user.id == bot.id
-        or random.random() < 0.1
-    ):
-        pepe_reply = generate_pepe_message(messages)
-        for name in USERNAMES.values():
-            pepe_reply = pepe_reply.replace(name, USERNAMES_TO_UKR.get(name))
+    # messages = [
+    #     {
+    #         "role": "user",
+    #         "name": USERNAMES.get(update.message.from_user.id, "Павло"),
+    #         # "content": f"{RANDOM_SINGLE_MESSAGE_USER_PROMPT} у відповідь на `{update.message.text}`",
+    #         "content": update.message.text,
+    #     }
+    # ]
+    # message = dict(
+    #     username=USERNAMES.get(update.message.from_user.id, "Павло"),
+    #     text=f"{RANDOM_SINGLE_MESSAGE_USER_PROMPT} у відповідь на `{update.message.text}`",
+    # )
+    message = dict(username=USERNAMES.get(update.message.from_user.id, "Павло"), text=update.message.text)
+    conversation = dynamodb_operations.get_conversation(update.message.chat_id)
+    if not conversation:
+        dynamodb_operations.create_conversation(chat_id=update.message.chat_id, **message)
+    else:
+        dynamodb_operations.append_conversation_message(
+            chat_id=update.message.chat_id,
+            **message,
+            pop_old_message=len(conversation.get("messages", [])) > GPT_CONTEXT_LENGTH
+        )
+
+    if send_message_triggered(update.message):
+        pepe_reply = generate_pepe_message(conversation.get("messages", []) + [message])
+        for name, ukr_name in USERNAMES_TO_UKR.items():
+            pepe_reply = pepe_reply.replace(name, ukr_name)
         bot.send_message(update.message.chat_id, text=pepe_reply)
 
     return {"statusCode": HTTPStatus.OK}
--- src/layers/main_layer/pepe.py
+++ src/layers/main_layer/pepe.py
@@ -1,10 +1,18 @@
-from gpt import complete_chat
+import re
+from typing import List
 
+from gpt import complete_chat
 
 SINGLE_MESSAGE_SYSTEM_PROMPT = """Act as an AI member of the Ukrainian friends group chat. 
 You are freaky, funny, interesting and a little rude companion 
 which likes saying crazy surprising, amusing, and sometimes stupid things using swearing."""
+PEPE_ALIASES = (re.compile(r"\sпепе\s"), re.compile(r"\spepe\s"), re.compile(r"\s@pepe_ukrainian_bot\s"))
 
 
-def generate_pepe_message(messages):
-    return complete_chat([{"role": "system", "content": SINGLE_MESSAGE_SYSTEM_PROMPT}, *messages])
+def generate_pepe_message(messages: List[dict]):
+    return complete_chat(
+        [
+            {"role": "system", "content": SINGLE_MESSAGE_SYSTEM_PROMPT},
+            *[{"role": "user", "name": msg["username"], "content": msg["text"]} for msg in messages],
+        ]
+    )
